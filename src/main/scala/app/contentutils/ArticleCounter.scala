b0a15404f62af4fe47947b98067ccf5d76367de5
David Furey <david.furey@guardian.co.uk>
2015-11-02 16:00:53 +0000

    simplify

--- src/main/scala/app/contentutils/ArticleCounter.scala
+++ src/main/scala/app/contentutils/ArticleCounter.scala
@@ -10,18 +10,17 @@ class ArticleCounter {
   val contentApiClient = new GuardianContentClient("api-key-here")
 
   def count = {
-    val itemQuery = {
-      val until = DateTime.now
-      val from = until.minusMinutes(15)
-      val q = new SearchQuery().fromDate(from).toDate(until)
-      q.withParameter(q.showBlocks.withValue(Some("all")))
-        .withParameter(q.showElements.withValue(Some("all")))
-        .withParameter(q.showFields.withValue(Some("all")))
-        .withParameter(q.showTags.withValue(Some("all")))
-        .withParameter(q.page.withValue(Some(1)))
-        .withParameter(q.pageSize.withValue(Some(100)))
-        .orderBy("oldest")
-    }
+    val until = DateTime.now
+    val from = until.minusMinutes(15)
+    val q = new SearchQuery().fromDate(from).toDate(until)
+    val itemQuery = q.withParameter(q.showBlocks.withValue(Some("all")))
+      .withParameter(q.showElements.withValue(Some("all")))
+      .withParameter(q.showFields.withValue(Some("all")))
+      .withParameter(q.showTags.withValue(Some("all")))
+      .withParameter(q.page.withValue(Some(1)))
+      .withParameter(q.pageSize.withValue(Some(100)))
+      .orderBy("oldest")
+
     contentApiClient.getResponse(itemQuery) map { response =>
       response.results.size
     }
