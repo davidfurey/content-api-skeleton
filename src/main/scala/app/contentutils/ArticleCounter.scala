f6f27c4f8bff2f1402f4c39a20eed638806a930a
David Furey <david.furey@guardian.co.uk>
2015-11-09 16:48:58 +0000

    Tidy up client use

--- src/main/scala/app/contentutils/ArticleCounter.scala
+++ src/main/scala/app/contentutils/ArticleCounter.scala
@@ -7,21 +7,23 @@ import org.joda.time.DateTime
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class ArticleCounter {
-  val contentApiClient = new GuardianContentClient("api-key-here")
+  val contentApiClient = new GuardianContentClient("kpfvwzq9xm3vfu22grbh9yqe")
 
   def count = {
     val until = DateTime.now
     val from = until.minusMinutes(15)
-    val q = new SearchQuery().fromDate(from).toDate(until)
-    val itemQuery = q.withParameter(q.showBlocks.withValue(Some("all")))
-      .withParameter(q.showElements.withValue(Some("all")))
-      .withParameter(q.showFields.withValue(Some("all")))
-      .withParameter(q.showTags.withValue(Some("all")))
-      .withParameter(q.page.withValue(Some(1)))
-      .withParameter(q.pageSize.withValue(Some(100)))
+    val searchQuery = new SearchQuery()
+      .fromDate(from)
+      .toDate(until)
+      .showBlocks("all")
+      .showElements("all")
+      .showFields("all")
+      .showTags("all")
+      .page(1)
+      .pageSize(100)
       .orderBy("oldest")
 
-    contentApiClient.getResponse(itemQuery) map { response =>
+    contentApiClient.getResponse(searchQuery) map { response =>
       response.results.size
     }
   }
